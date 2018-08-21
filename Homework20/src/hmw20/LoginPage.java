518ae57da90d23fcd7c945c3ace1b3f04747d72a
StassiaSt <agstelmak@gmail.com>
2018-08-21 22:55:04 +0300

    added Homework20

---
+++ Homework20/src/hmw20/LoginPage.java
@@ -0,0 +1,46 @@
+package hmw20;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+	
+	   @FindBy(id = "mailbox:login")
+	    private WebElement loginField;
+
+	    @FindBy(id = "mailbox:password")
+	    private WebElement emailField;
+
+	    @FindBy(xpath = "//*[@id='mailbox:submit']/input")
+	    private WebElement buttonEnter;
+
+	    @FindBy(xpath = "//*[@id='PH_logoutLink']")
+	    private WebElement logoutLink;
+
+	    public LoginPage(WebDriver webdriver)
+	    {
+	        PageFactory.initElements(webdriver, this);
+	    }
+
+	    public void enterLoginAndPass(String login, String password)
+	    {
+	        loginField.clear();
+	        loginField.sendKeys(login);
+	        emailField.clear();
+
+	    }
+
+	    public void clickEnterButton()
+	    {
+	        buttonEnter.click();
+	    }
+
+	    public boolean logoutLinkPresents()
+	    {
+	        return logoutLink.isDisplayed();
+	    }
+	    
+	   
+	}
